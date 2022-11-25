67190411dcb715d4d25af22f9e6fd0422488d90f
MONSTER <MONSTER@BERKE>
2022-11-26 00:07:25 +0300

    Proje dosyası ekle.

---
+++ MYAZ303-ODEV/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace MYAZ303_ODEV.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ MYAZ303-ODEV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MYAZ303_ODEV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
